b8fc9df9cf4f271f206638f0f9ac3a05ecda0f57
Terence Wayburne <twayburne@gmail.com>
2018-09-20 17:32:01 +0800

    start new terry

--- start.js
+++ start.js
@@ -1,17 +1,1 @@
-// VARIABLES
-
-const terry = "KING"; //new CONSTANT - locked
-let points = 0; //new unlocked variable
-
-
-
-
-
-function score() {
-	points += 1;
-}
-
-function changeName() {
-	terry = "Emperor";
-}
 
